7fd76edb27101f77b4838dd32f0936173bf2e7ec
abmcbride1 <abmcbride@jasperhelps.email>
2021-06-07 06:26:19 -0700

    updated styling for header and sources.

--- src/components/sources.js
+++ src/components/sources.js
@@ -9,9 +9,12 @@ export default function SourcesComp(){
         <div className="sources">
             <h1>Sources</h1>
             <p><strong>All sources used to supply info porvided by this site are listed below:</strong></p>
+            <h3>Tiles: </h3>
            <Source link="https://orbus.online/index.php?title=Tiles" />
            <Source link="https://orbusfreepress.org/index.php/Artificing" />
+           <h3>Fishing: </h3>
            <Source link="https://orbusfreepress.org/index.php/Fishing" />
+           <h3>Dungeons: </h3>
            <Source link="https://orbusvr.fandom.com/wiki/Dungeons" />
            <Source link="https://orbusfreepress.org/index.php/Dungeons" />
            <Source link="https://orbus.online/index.php?title=Shard_Dungeons#Modifiers" />
